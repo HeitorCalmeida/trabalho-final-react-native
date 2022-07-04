f691f654b831d8a5507c25ef7d88bb9d467d41af
feittosaa <lucas.feitosa2004@gmail.com>
2022-07-04 16:55:20 -0300

    .

--- src/screens/PagInicial/index.tsx
+++ src/screens/PagInicial/index.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import styles from "./style"
 
 export const PagInicial = () => {
     return (
         <View style={styles.container}>
             <Text>Projeto React Native Grupo 2</Text>
-            <StatusBar style="auto"/>
+            <StatusBar style="auto" />
         </View>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#ff8',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
