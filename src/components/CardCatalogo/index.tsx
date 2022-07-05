09708b58ebe89adc73620f661224e3e84a7649b0
Gabriel Fernandes <gaabezk@gmail.com>
2022-07-05 15:00:59 -0300

    implementar axios

---
+++ src/components/CardCatalogo/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+import api from "../../services/api";
+
+export const CardCatalogo = () => {
+
+    return(
+        <View>
+            
+        </View>
+    )
+}
