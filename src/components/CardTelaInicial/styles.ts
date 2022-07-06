e3e9e6a2d72cd78931f763b13bb5bfdaf88240c7
RenanLopes1 <renannfl1@gmail.com>
2022-07-05 21:25:24 -0300

    telainicial

---
+++ src/components/CardTelaInicial/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  
+    container:{
+        backgroundColor: '#1A1A1A',
+        borderRadius: 10,
+        width: 170,
+        height: 170,
+        marginLeft: 25,
+        marginVertical: 10,
+    },
+    texto1:{
+        color:'#fff',
+        alignSelf:'center',
+    }
+});
+
